9fbb6265c3e321baf3a3c1590673e57bbedc7c24
Asemtko <112264285+Asemtko@users.noreply.github.com>
2024-10-19 12:02:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import React, { useState } from "react";
+import Field from "./components/field";
+import Translate from "./components/translate";
+import Languages from "./components/languages";
+
+export default function App() {
+  const [language, setLanguage] = useState("");
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <Field onChange={setText} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <br />
+      <Translate text={text} language={language} />
+    </div>
+  );
+}
